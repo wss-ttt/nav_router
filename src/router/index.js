7b5ddf649a9672c2856e551abf6df953df379201
王水胜 <602466011@qq.com>
2019-07-29 16:52:30 +0800

    更新：菜单 router 跳转

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,40 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+  	{
+  		path:'/',
+  		redirect: '/home'
+  	},
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+    	path:'/',
+    	name:'main',
+    	component: resolve => require(['@/views/main.vue'], resolve),
+    	children:[
+    		{
+    			path: '/home',
+    			name:'home',
+          component: resolve => require(['@/components/home.vue'], resolve),
+    		},
+    		{
+    			path: '/blog',
+    			name:'blog',
+          component: resolve => require(['@/components/blog.vue'], resolve),
+    		},
+    		{
+    			path: '/news',
+    			name:'news',
+          component: resolve => require(['@/components/news.vue'], resolve),
+    		},
+    		{
+    			path: '/photo',
+    			name:'photo',
+          component: resolve => require(['@/components/photo.vue'], resolve),
+    		},
+    	]
     }
   ]
 })
