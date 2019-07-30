64628e5c3a1b9aa9c2c9b5c346f34215d820ccef
王水胜 <602466011@qq.com>
2019-07-30 11:26:41 +0800

    更新：添加common.js代码

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -1,7 +1,15 @@
 export default {
   namespaced: true,
   state: {
+  	mainTabs:[],
+  	mainTabsActiveName:''
   },
   mutations: {
+  	updateMainTabs (state, tabs) {
+      state.mainTabs = tabs
+    },
+    updateMainTabsActiveName (state, name) {
+      state.mainTabsActiveName = name
+    }
   }
 }
