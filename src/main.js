488c1b905ce7bd65292be419187d13d54e7e6b6c
王水胜 <602466011@qq.com>
2019-07-30 11:23:57 +0800

    更新：引入vuex的使用

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import store from './store/index.js'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
@@ -12,6 +13,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
--- src/views/main.vue
+++ src/views/main.vue
@@ -1,13 +1,15 @@
 <template>
 	<div class="wrapper">
 		<div class="sidebar">
-			<el-menu class="sidebar-el-menu"
-			background-color="#545c64" 
-			text-color="#fff" 
-			active-text-color="#ffd04b"
-			:default-active="this.$route.path" 
-			router>
-				<el-menu-item v-for="(item,index) in items" :index="item.index" :key="item.index">
+			<el-menu class="sidebar-el-menu" 
+				background-color="#545c64" 
+				text-color="#fff" 
+				active-text-color="#ffd04b" 
+				:default-active="this.$route.path" 
+				router>
+				<el-menu-item v-for="(item,index) in items" 
+					:index="item.index" 
+					:key="item.index">
 					<i :class="item.icon"></i>
 					<span>{{item.title}}</span>
 				</el-menu-item>
@@ -19,51 +21,61 @@
 	</div>
 </template>
 <script>
-	export default{
-		name:'',
-		data(){
-			return{
-				items:[
-					{
-						icon:'el-icon-menu',
-						index:'/home',
-						title:'首页'
+	export default {
+		name: '',
+		data() {
+			return {
+				items: [{
+						icon: 'el-icon-menu',
+						index: '/home',
+						title: '首页'
 					},
 					{
-						icon:'el-icon-document',
-						index:'/photo',
-						title:'相册'
+						icon: 'el-icon-document',
+						index: '/photo',
+						title: '相册'
 					},
 					{
-						icon:'el-icon-setting',
-						index:'/blog',
-						title:'博客'
+						icon: 'el-icon-setting',
+						index: '/blog',
+						title: '博客'
 					},
 					{
-						icon:'el-icon-location',
-						index:'/news',
-						title:'新闻'
+						icon: 'el-icon-location',
+						index: '/news',
+						title: '新闻'
 					},
 				]
 			}
 		},
+		watch:{
+			$route: 'routeHandle'
+		},
+		methods:{
+			routeHandle(route){
+				console.log('我这个执行了没');
+				console.log(route);
+			}
+		}
 	}
 </script>
 <style>
-	.sidebar{
+	.sidebar {
 		/*width: 250px;*/
 		position: absolute;
-		top:0;
+		top: 0;
 		left: 0;
 		bottom: 0;
 		border: 1px solid #ddd;
 	}
+	
 	.sidebar-el-menu:not(.el-menu--collapse) {
 		width: 250px;
 	}
-	.content{
+	
+	.content {
 		position: absolute;
-		top:0;
+		top: 0;
 		left: 250px;
 		right: 0;
 		bottom: 0;
