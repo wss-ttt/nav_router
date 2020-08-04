3e4841f1574448972851fe600df708e8414dea07
wangshuisheng <602466011@qq.com>
2020-08-04 20:41:38 +0800

    更新: 添加新增和修改操作

--- src/views/modules/Table.vue
+++ src/views/modules/Table.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="wrapper">
     <div class="query">
-      <el-form :inline="true" :model="dataForm">
+      <el-form :inline="true" :model="queryParams">
         <el-form-item label="审批人">
-          <el-input v-model="dataForm.name" placeholder="用户名"></el-input>
+          <el-input v-model="queryParams.name" placeholder="用户名"></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="query">查询</el-button>
-          <el-button type="primary" @click="openDialog">新增</el-button>
+          <el-button type="primary" @click="handleAdd">新增</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -26,18 +26,18 @@
 
     <!-- 新增和修改 -->
     <el-dialog :title="title" :visible.sync="open" width="30%">
-      <el-form ref="dataForm2" :model="dataForm2" label-width="80px">
+      <el-form ref="form" :model="form" label-width="80px">
         <el-form-item label="用户名">
-          <el-input v-model="dataForm2.name"></el-input>
+          <el-input v-model="form.name"></el-input>
         </el-form-item>
         <el-form-item label="性别">
-          <el-radio-group v-model="dataForm2.sex">
+          <el-radio-group v-model="form.sex">
             <el-radio label="男"></el-radio>
             <el-radio label="女"></el-radio>
           </el-radio-group>
         </el-form-item>
         <el-form-item label="年龄">
-          <el-input v-model.number="dataForm2.age"></el-input>
+          <el-input v-model.number="form.age"></el-input>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -49,70 +49,117 @@
 </template>
 
 <script>
-import { getUserList, removeUser } from '@/api/user.js'
-export default {
-  components: {},
-  props: {},
-  data() {
-    return {
-      dataForm: {
-        name: ''
-      },
-      dataForm2: {}, // 新增和修改
-      list: [],
-      title: '', // 弹出框的标题
-      open: false // 新增和修改弹出框的显示和隐藏
-    }
-  },
-  computed: {},
-  watch: {},
-  created() {},
-  mounted() {
-    this.getList()
-  },
-  activated() {},
-  deactivated() {},
-  updated() {},
-  destroyed() {},
-  methods: {
-    getList() {
-      getUserList().then(res => {
-        this.list = res.data
-      })
+  import {
+    getUserList,
+    removeUser,
+    getUserInfo,
+    editUser,
+    addUser
+  } from '@/api/user.js'
+  export default {
+    components: {},
+    props: {},
+    data() {
+      return {
+        queryParams: { // 查询参数
+          name: ''
+        },
+        form: {}, // 新增和修改
+        list: [],
+        title: '', // 弹出框的标题
+        open: false // 新增和修改弹出框的显示和隐藏
+      }
     },
-    handleDelete(row) {
-      const id = row.id
-      this.$confirm('是否删除当前数据', '警告', {
-        confirmButtonText: '确 定',
-        cancelButtonText: '取 消',
-        type: 'warning'
-      })
-        .then(() => {
-          // 这里需要return 也就是返回的时要给Promise对象
-          return removeUser({ id: id })
-        })
-        .then(() => {
-          this.getList()
-          this.$message.success('删除成功')
-        })
-    },
-    handleUpdate() {},
-    query() {
+    computed: {},
+    watch: {},
+    created() {},
+    mounted() {
       this.getList()
     },
-    openDialog() {
-      this.open = true
-      this.title = '新增'
+    activated() {},
+    deactivated() {},
+    updated() {},
+    destroyed() {},
+    methods: {
+      getList() {
+        getUserList().then(res => {
+          this.list = res.data
+        })
+      },
+      handleDelete(row) {
+        const id = row.id
+        this.$confirm('是否删除当前数据', '警告', {
+            confirmButtonText: '确 定',
+            cancelButtonText: '取 消',
+            type: 'warning'
+          })
+          .then(() => {
+            // 这里需要return 也就是返回的时要给Promise对象
+            return removeUser({
+              id: id
+            })
+          })
+          .then(() => {
+            this.getList()
+            this.$message.success('删除成功')
+          })
+      },
+      query() {
+        this.getList()
+      },
+      handleAdd() {
+        this.open = true
+        this.title = '新增'
+      },
+      async handleUpdate(row) {
+        const id = row.id
+        const res = await getUserInfo({
+          id: id
+        })
+        this.form = res.data
+        this.title = '修改'
+        this.open = true
+      },
+      cancel() {
+        this.open = false
+      },
+      submitForm() {
+        if(this.form.id !== undefined) {
+          // 修改操作
+          editUser(this.form).then(res => {
+            if(res.code === 0) {
+              this.getList()
+              this.$message.success('修改成功')
+              this.open = false
+            } else {
+              this.$message.error('修改失败,请重试')
+              this.open = false
+            }
+          }).catch(error => {
+            this.$message.error('请求异常,请重试')
+            this.open = false
+          })
+        } else {
+          // 新增操作
+          addUser(this.form).then(res => {
+            if(res.code === 0) {
+              this.getList()
+              this.$message.success('添加成功')
+              this.open = false
+            } else {
+              this.$message.error('添加事变,请重试')
+              this.open = false
+            }
+          }).catch(error => {
+            this.$message.error('请求异常,请重试')
+            this.open = false
+          })
+        }
+      }
     },
-    cancel() {
-      this.open = false
-    },
-    submitForm() {
-      
-    }
-  },
-  filters: {}
-}
+    filters: {}
+  }
+
 </script>
 <style scoped lang="scss">
 </style>
