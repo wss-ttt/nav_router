3221e03f2832bf71a1eb79383fe12c1bd86e1b2f
王水胜 <602466011@qq.com>
2020-08-04 14:51:40 +0800

    更新: user.js 接口

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,23 @@
 import request from '@/utils/request'
+
 export function login(data) {
   return request({
     url: '/api/login',
+    method: 'post',
+    data: data
+  })
+}
+
+export function getUserList() {
+  return request({
+    url: '/api/user/list',
+    method: 'post'
+  })
+}
+
+export function removeUser(data) {
+  return request({
+    url: '/api/user/remove',
     method: 'post',
     data: data
   })
