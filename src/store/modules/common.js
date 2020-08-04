b23d92fb6289c61588e0fffb542830420ece2e9d
王水胜 <602466011@qq.com>
2020-08-04 09:57:06 +0800

    格式化

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -1,36 +1,35 @@
 export default {
   namespaced: true,
   state: {
-  	documentClientHeight:'',
-  	mainTabs:[],
-    mainTabsActiveName:'',
+    documentClientHeight: "",
+    mainTabs: [],
+    mainTabsActiveName: "",
     sidebarFold: false, // 侧边栏的折叠状态
-    documentClientHeight: '', // 可视区域的高度
-    name: '', // 保存登录名
-    hasLogin: false, // 表示是否已经登录了
+    documentClientHeight: "", // 可视区域的高度
+    name: "", // 保存登录名
+    hasLogin: false // 表示是否已经登录了
   },
   mutations: {
-  	updateMainTabs (state, tabs) {
+    updateMainTabs(state, tabs) {
       state.mainTabs = tabs;
     },
-    updateMainTabsActiveName (state, name) {
+    updateMainTabsActiveName(state, name) {
       state.mainTabsActiveName = name;
     },
-    updateDocumentClientHeight(state,documentClientHeight){
-    	console.log('updateDocumentClientHeight--mutations');
-    	state.documentClientHeight = documentClientHeight;
+    updateDocumentClientHeight(state, documentClientHeight) {
+      state.documentClientHeight = documentClientHeight;
     },
     updateSidebarFold(state, sidebarFold) {
-      state.sidebarFold = sidebarFold
+      state.sidebarFold = sidebarFold;
     },
     updateDocumentClientHeight(state, documentClientHeight) {
-      state.documentClientHeight = documentClientHeight
+      state.documentClientHeight = documentClientHeight;
     },
     updateName(state, name) {
-      state.name = name
+      state.name = name;
     },
     updateHasLogin(state, hasLogin) {
-      state.hasLogin = hasLogin
+      state.hasLogin = hasLogin;
     }
   }
-}
+};
