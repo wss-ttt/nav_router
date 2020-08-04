d13fed3884d5c87c73d0a9ea5c5dcf1070ad165a
wangshuisheng <602466011@qq.com>
2020-08-04 21:59:42 +0800

    添加: echarts-settings.js

---
+++ src/echarts-settings.js
@@ -0,0 +1,12 @@
+export default {
+  loading: {
+    text: '',
+    textColor: '',
+    // spinnerRadius: 30,
+    maskColor: 'rgba(255,255,255,0)'
+  },
+  seriesColors: ['#FFC45C', '#4BACFE', '#B036EF', '#EA3B3B', '#F39B44', '#3195E9', '#A069E4', '#E45B6B', '#EE8131',
+    '#2970E6', '#672BB2', '#C84E7D', '#C56E18', '#3F43C9', '#6904E5', '#FC3A76', '#5480e5', '#E66221',
+    '#4872D3', '#7E2CE3', '#CF330F', '#2859CB', '#723B8E', '#83283E', '#FFC45C', '#4BACFE', '#B036EF', '#EA3B3B', '#F39B44', '#3195E9', '#A069E4', '#E45B6B', '#EE8131'
+  ]
+}
