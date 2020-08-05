dedcf5fa0f5c4d217d7257678e62b5366f1906de
王水胜 <602466011@qq.com>
2020-08-05 10:52:59 +0800

    更新: 添加地图的其他配置

--- src/views/modules/Map.vue
+++ src/views/modules/Map.vue
@@ -23,15 +23,55 @@ export default {
     const vm = this
     this.myChart = echarts.init(document.getElementById('map'))
     getMapData().then(res => {
+      // 1.registerMap
       echarts.registerMap('hubei', res.data)
+      // 2.给每个区随机产生一点数据(悬浮提示显示的就是该数据)
+      let data = res.data.features.map(item => {
+        return {
+          name: item.properties.name,
+          value: Math.round(Math.random() * 100)
+        }
+      })
       this.myChart.setOption({
         title: {
           text: '湖北省地图',
           x: 'center'
         },
+        tooltip: {
+          backgroundColor: 'rgba(0,0,0,1)', //提示标签背景颜色
+          textStyle: {
+            color: '#fff'
+          }
+        },
         series: [{
           type: 'map',
-          map: 'hubei'
+          map: 'hubei',
+          data: data,
+          label: {
+              normal: {
+                show: true, //显示省份标签
+                textStyle:{color:"#c71585"}//省份标签字体颜色
+              },
+              emphasis: {
+                show: true, //对应的鼠标悬浮效果
+                textStyle: {
+                  color: '#800080',
+                  fontSize: 18
+                }
+              }
+            },
+            itemStyle: {
+              normal: {
+                borderWidth: 0.5, //区域边框宽度
+                borderColor: '#009fe8', //区域边框颜色
+                areaColor: '#ffefd5' //区域颜色
+              },
+              emphasis: {
+                borderWidth: 0.5,
+                borderColor: '#4b0082',
+                areaColor: '#ffdead'
+              }
+            }
         }]
       })
     })
