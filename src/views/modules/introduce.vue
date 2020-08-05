dc1901826ae9607d6b86034606deda7692e4ca90
wangshuisheng <602466011@qq.com>
2020-08-05 20:13:55 +0800

    更新: 添加项目介绍说明

--- src/views/modules/introduce.vue
+++ src/views/modules/introduce.vue
@@ -2,8 +2,17 @@
 	<div class="introduce">
 		<h2>项目介绍</h2>
 		<ul>
-			<li>vue+element-ui</li>
-			<li>router vuex</li>
+			<li>前端:</li>
+			<li>1.vue+vuex+vue-router+axiso搭建项目</li>
+			<li>2.使用element-ui搭建ui界面</li>
+			<li>3.使用echarts进行数据的可视化展示</li>
+			<li>4.前端主要包含了后端管理系统基本的目录结构和基本操作流程及核心文件</li>
+			<li>5.地址:https://github.com/wss-ttt/nav_router.git</li>
+			<li>后端:</li>
+			<li>1.使用express+mysql搭建服务,实现基本的curd操作</li>
+			<li>2.地址:https://github.com/wss-ttt/nav_router_back.git</li>
+			<li>tips:</li>
+			<li>好好学习 天天向上</li>
 		</ul>
 	</div>
 </template>
