9826640faacfa2fed4140098e77530db4882f249
王水胜 <602466011@qq.com>
2020-08-05 14:36:43 +0800

    添加: other.vue 页面

---
+++ src/views/modules/other.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="wrapper">
+       世界上有三样东西是别人抢不走的
+       <br>
+       1.读进大脑里的书
+       <br>
+       2.藏进心中的梦想
+       <br>
+       3.吃进胃里的食物
+       <br>
+       ...所以，要做一个有梦想的 、爱读书的吃货
+   </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    props: {},
+    data() {
+      return {
+          
+      }
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    mounted() {},
+    activated() {},
+    deactivated() {},
+    updated() {},
+    destroyed() {},
+    methods: {},
+    filters: {}
+  }
+</script>
+<style scoped>
+</style>
