328a9d1a4cb29dca8ccd2bb60a8b175f0dce0ffd
王水胜 <602466011@qq.com>
2020-09-23 09:29:53 +0800

    更新: 全局路由守卫拦截

--- src/main.js
+++ src/main.js
@@ -15,8 +15,16 @@ Vue.config.productionTip = false
 router.beforeEach((to, from, next) => {
   const token = sessionStorage.getItem('hasLogin')
   if (token) {
-    next()
+    // 有token(也就是已经登录了)
+    if(to.path === '/login') {
+      // 在已经登录的情况下,再来访问login页面,就直接跳转到首页去
+      next({ path: '/'})
+    } else {
+      // 在已经登录的情况下,访问其他其他页面,就直接进入到其他页面中去
+      next()
+    }
   } else {
+    // 没有token(s)
     if (to.path === '/login') {
       next()
     } else {
