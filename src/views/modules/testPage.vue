7c677dd9187f64f6e9f7eecf67757137911d8fe7
王水胜 <602466011@qq.com>
2020-09-29 10:09:59 +0800

    添加: testPage.vue

---
+++ src/views/modules/testPage.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="wrapper">
+       aaa
+   </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    props: {},
+    data() {
+      return {
+          
+      }
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    mounted() {},
+    activated() {},
+    deactivated() {},
+    updated() {},
+    destroyed() {},
+    methods: {},
+    filters: {}
+  }
+</script>
+<style scoped>
+</style>
